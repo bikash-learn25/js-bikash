531403c5dcbb15d9a48741e2ddb57e822daa5237
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-15 16:57:42 +0000

    container config done

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -3,3 +3,23 @@
 // alert(2+3); // we're using nodejs not browser
 
 console.log(2+3)
+
+let name = "Bikash"
+let age = 25
+let isLoggedIn = false
+let state;
+
+//Primitive data types
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => symbol is used to find an uniqueness
+
+//object
+
+console.log(typeof isLoggedIn) //boolean
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
