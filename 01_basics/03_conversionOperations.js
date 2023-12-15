2cc75eeeb439f425ae08544f30070628f3268ade
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-15 17:30:27 +0000

    container config done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+// let score = "33" //o/p - 33
+// let score = "Bikash" //o/p - NaN
+// let score = null //o/p - 0
+// let score = undefined //o/p - NaN
+// let score = "33abc" // o/p - NaN
+// let score = true // o/p - 1
+// let score = false // o/p - 0
+
+// console.log(typeof score) //number
+// console.log(typeof (score)) //number
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) //NaN
+
+let isLoggedIn = 1 //true
+// let isLoggedIn = 0 //false
+// let isLoggedIn = "" //false
+// let isLoggedIn = "Bikash" //true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
