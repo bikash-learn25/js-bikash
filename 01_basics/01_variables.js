a1f53e65cd50e8871eb64cae6a8cfee810b9fe58
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-15 16:02:25 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,3 +13,12 @@ console.log(accountId);
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
 
+/*
+The keyword "var" is less preferrable used and most widely used keywords are "let" and "const".
+; because of issue in block scope and functional scope.
+
+- Variables declared with "var" are function-scoped, not block-scoped. 
+- This means that the variable is visible throughout the entire function, even if it's declared inside a block.
+
+*/
+
