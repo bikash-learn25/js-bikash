b65ec6db2914ebed1644cdbbae7b2af8827c960d
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-15 15:19:36 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,13 @@ let accountEmail = "bkda.dev1@gmail.com";
 var accountPassword = "12345";
 accountCity = "Balasore";
 
-accountId = 2;
+// accountId = 2; // not allowed
+
+accountEmail = "bk%bk.com"
+accountPassword = "212121"
+accountCity = "BBSR"
 
 console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
