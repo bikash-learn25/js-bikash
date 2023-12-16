7d5be9a65a66c0ec7634b007ef22fe79fddf07fb
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-16 12:39:43 +0000

    Comparisons converts done

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,6 +1,16 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+console.log(undefined > 0) //false
+console.log(undefined == 0) //false
+console.log(undefined >= 0) //false
+
+// "===" -> Strict check, returns always a boolean result (true/false)
+
+console.log(2 === 2) //true
+console.log(2 === 12) //false
+
