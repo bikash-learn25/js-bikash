fc8201f6be6dd1ca2bba52f98aaa5bb2b843cace
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-16 13:44:39 +0000

    datatypes conversions

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,40 @@
+// Primitive 
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+
+// console.log(id === anotherId)
+// console.log(id == anotherId)
+
+const bigNumber = 87346734809347634834n
+
+// console.log(bigNumber)
+
+
+// Reference type or Non-Primitives
+
+// 3 types: Objects, Arrays, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Bikash",
+    age: 25,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber)
+
+
+
+
+
+
+
+
+
+
