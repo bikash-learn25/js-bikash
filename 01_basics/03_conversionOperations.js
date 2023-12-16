d008a041390a28099bd6c461c4010da2a69b9ffc
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-16 12:11:26 +0000

    conversions ops done

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -44,3 +44,51 @@
 
 // Concept 5:
 
+// let str1 = "hello"
+// let str2 = "Bikash"
+
+// let str3 = str1 + str2
+
+// // console.log(str3)
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2) //"1" + 2: The string "1" is concatenated with the number 2, resulting in the string "12"
+//                         //"12" + 2: The string "12" is concatenated with the number 2, resulting in the string "122"
+
+// console.log(1 + 2 + "2") //1 + 2: The numbers 1 and 2 are added, resulting in the number 3
+//                         //3 + "2": The number 3 is implicitly converted to a string, and then it is concatenated with the string "2", resulting in the string "32"
+
+
+// Concept 6:
+
+// console.log((2+3) * 5 % 2); //code redeability should be Prioritized
+console.log(true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+// console.log(num2)
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+///link to study: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+// let x = 3;
+// const y = x++;
+// // x is 4; y is 3
+
+// let x2 = 3n;
+// const y2 = x2++;
+// // x2 is 4n; y2 is 3n
+
+// console.log(x, y, x2, y2)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+
