263fadafcbe9c63d91cca1f129d3bf412807ea63
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-20 06:04:12 +0000

    maths ops doen

---
+++ 01_basics/06_numsAndMaths.js
@@ -0,0 +1,22 @@
+const score = 400
+console.log(score)
+
+const balance = new Number(100) // Number() is an object wrapper for primitive numeric values
+
+// console.log(balance) // 100
+console.log(Number()) // 0
+
+console.log(balance.toString().length) 
+
+// 3: Converts the numeric value balance to its string representation.
+// As balance is 100, it will log the length of the string "100", which is 3, since there are three characters in the string.
+
+console.log(balance.toFixed(2)) // 100.00
+// It ensures that the number is represented with two decimal places, even if there are no decimal values in the original number. Especially used in E-commerce site
+
+//JavaScript to directly use primitive values for numbers rather than creating instances of the Number object. 
+
+// const balance = 100; // This is the widely used way to declare ratger than new Number()
+
+// console.log(balance);
+
