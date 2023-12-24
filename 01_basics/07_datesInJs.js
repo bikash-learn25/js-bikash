0c58ff3344b796a6b1f105f69e7e58af917b7a48
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-24 13:39:34 +0000

    bare metal servers discussion done

---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,19 @@
+// let myDate = new Date()
+
+// console.log(typeof myDate) // Object
+
+// console.log(myDate.toString()) // Sun Dec 24 2023 13:01:20 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toDateString()) // Sun Dec 24 2023
+let myNewDate = new Date(2023, 2, 12)
+console.log(myNewDate.toDateString()) // Sun Mar 12 2023
+
+// console.log(myDate.toISOString()) //2023-12-24T13:04:03.139Z
+
+// console.log(myDate.toLocaleDateString()) // 12/24/2023
+
+// console.log(myDate.toLocaleString()) // 12/24/2023, 1:04:03 PM
+
+// console.log(myDate.toTimeString()) // 13:04:03 GMT+0000 (Coordinated Universal Time)
+
+// console.log(myDate.toJSON()) // 2023-12-24T13:04:03.139Z
---
+++ 01_basics/bareMetalServer.js
@@ -0,0 +1,23 @@
+// https://www.ibm.com/topics/bare-metal-dedicated-servers
+
+/*
+
+What is a bare metal server?
+Bare metal servers are a form of cloud services in which the user rents a physical machine from a provider that is not shared with any other tenants.
+
+Unlike traditional cloud computing, which is based on virtual machines, bare metal servers do not come with a hypervisor pre-installed and give the user complete control over their server infrastructure.
+
+With a bare metal server, because users get complete control over the physical machine, they have the flexibility to choose their own operating system, avoid the “noisy neighbor” challenges of shared infrastructure, and finely tune hardware and software for specific, often data-intensive, workloads.
+
+Along with virtual machines, networking, and storage, bare metal servers are a foundational component of the infrastructure-as-a-service (IaaS) stack in cloud computing.
+
+*/
+
+/*
+
+Bare metal vs. dedicated servers
+The terms 'bare metal server' and 'dedicated server' are sometimes used interchangeably, and bare metal servers are in fact dedicated services. But while they are similar, they are not synonymous. The difference is less about the servers themselves, and more about how they are delivered by the service provider.
+
+Historically, dedicated servers have been associated with long provisioning times, billing increments of months or years, and often low-end or even dated hardware.
+
+*/
