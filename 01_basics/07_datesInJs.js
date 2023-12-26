9e338abfda1263260fb9acc6bb9fe686da5adac6
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-26 11:40:54 +0000

    array operations done

--- 01_basics/07_datesInJs.js
+++ 01_basics/07_datesInJs.js
@@ -5,15 +5,30 @@
 // console.log(myDate.toString()) // Sun Dec 24 2023 13:01:20 GMT+0000 (Coordinated Universal Time)
 
 // console.log(myDate.toDateString()) // Sun Dec 24 2023
-let myNewDate = new Date(2023, 2, 12)
-console.log(myNewDate.toDateString()) // Sun Mar 12 2023
+// let myNewDate = new Date(2023, 2, 12)
 
-console.log(myDate.toISOString()) //2023-12-24T13:04:03.139Z
+// console.log(myNewDate.toDateString()) // Sun Mar 12 2023
+
+// console.log(myDate.toISOString()) //2023-12-24T13:04:03.139Z
 
 // console.log(myDate.toLocaleDateString()) // 12/24/2023
 
 // console.log(myDate.toLocaleString()) // 12/24/2023, 1:04:03 PM
+// let myLocaleDate = new Date(2023, 2, 12, 2, 14, 3)
+// console.log(myLocaleDate.toLocaleString()) // 3/12/2023, 2:14:03 AM
 
 // console.log(myDate.toTimeString()) // 13:04:03 GMT+0000 (Coordinated Universal Time)
 
 // console.log(myDate.toJSON()) // 2023-12-24T13:04:03.139Z
+
+// let myCreateDate = new Date("01-14-2023")
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(myCreateDate.getTime())
+
+let newDate = new Date()
+
+console.log(newDate)
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDay())
---
+++ 02_basics/01_array.js
@@ -0,0 +1,13 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+// const myCars = ['BMW', 'TATA', 'TVS']
+
+// console.log(myCars)
+
+// console.log(myArr[3])
+
+// Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+console.log(myArr)
