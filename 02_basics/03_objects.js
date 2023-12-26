7a57b1cbf060da473a9cb6b06daf0dbcd73047af
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-26 18:15:40 +0000

    Two ways to declare an object: 1) Literal and 2) Constructor

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,32 @@
+// Two ways to declare an object: 1) Literal and 2) Constructor
+
+// Singleton object
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Bikash",
+    "full name": "Bikash Ad",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Balasore",
+    email: "bikash@gail.com",
+    isLoggedIn: false,
+    lastLogInDays: ["Monday", "Tuesday"]
+}
+
+console.log(jsUser.email) // bikash@gail.com
+
+// using two ways, an email can be obtained
+
+console.log(jsUser["email"]) // bikash@gail.com
+
+console.log(jsUser["full name"]) // Bikash Ad
+
+console.log(jsUser[mySym]) // mykey1
+
+console.log(typeof mySym) // symbol datatype
+
+Object.freeze(jsUser)
+console.log(jsUser)
+
