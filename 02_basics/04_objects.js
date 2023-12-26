3b59e112063f80a3ad7e2f37d2d11663633c239f
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2023-12-26 18:49:11 +0000

    objects are merged using spread method as well!

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -23,6 +23,7 @@ const regularUser = {
 
 const object1 = {1: "a", 2: "b"}
 const object2 = {3: "c", 4: "d"}
+const object4 = {5: "e", 6: "f"}
 
 // Two ways to merge objects:
 
@@ -30,5 +31,12 @@ const object2 = {3: "c", 4: "d"}
 
 // console.log(obj3) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
 
-const obj3 = Object.assign({}, object1, object2)
-console.log(obj3) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+// const obj3 = Object.assign({}, object1, object2) // // { '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+// console.log(obj3)
+
+// const obj3 = Object.assign({}, object1, object2, object4) // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' }
+
+const obj3 = {...object1, ...object2, ...object4} // { '1': 'a', '2': 'b', '3': 'c', '4': 'd', '5': 'e', '6': 'f' } - Mostly we will use this(spread) way to merge objects
+
+console.log(obj3) 
