d496052aafa05b74f53907d3e88d34d1118ae129
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2024-01-10 11:34:57 +0000

    forIn loop and obejcts and array are discussed!

---
+++ 04_iterations/four.js
@@ -0,0 +1,17 @@
+// const myObject = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     rb: 'ruby'
+// }
+
+// for (const key in myObject) {
+// //    console.log(myObject[key]);
+// console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+
+const programming = ['js', 'rb', 'c', 'cpp']
+
+for (const key in programming) {
+    console.log(key);
+}
--- 04_iterations/three.js
+++ 04_iterations/three.js
@@ -1,3 +1,41 @@
 // High Order Array loops
 
-// for 
+// for of loop
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num); //
+// }
+
+// const greetings = "Hello world"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+// }
+
+
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('US', "USA")
+// map.set('FR', "France")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, ':-', value);
+// }
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spider-Man'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
+
