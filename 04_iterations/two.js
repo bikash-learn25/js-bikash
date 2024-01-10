a0eaa4cbc740e7c0fc1149114929c45cda04ebbe
Bikash Das Adhikari <123795351+bikash-learn25@users.noreply.github.com>
2024-01-10 06:41:15 +0000

    while loop with array ops

---
+++ 04_iterations/two.js
@@ -0,0 +1,15 @@
+// while loop
+
+let i = 0
+while (i <= 10) {
+    console.log(`value of index is ${i}`);
+    i = i + 2
+}
+
+let myArr = ['a', 'b', 'c']
+
+let arr = 0
+while (arr < myArr.length) {
+    console.log(`value is ${myArr[arr]}`);
+    arr = arr + 1 // arr++
+}
